01dc6a19d2809236787351a8b82c2f60a9731e53
unknown <saulodahaduken@gmail.com>
2022-11-23 14:51:01 -0300

    AlgoritimoTodosCaminhos

---
+++ IFES-SI-TPA-Trabalho01-2022/src/br/edu/ifes/si/tpa/AlgoritimoTodosCaminhos.java
@@ -0,0 +1,59 @@
+package br.edu.ifes.si.tpa;
+
+public class AlgoritimoTodosCaminhos {
+
+    private boolean[] noCaminho;
+    private Pilha<Integer> caminho;
+
+    public AlgoritimoTodosCaminhos(Digrafo digrafo, int vo, int vd) {
+        noCaminho = new boolean[digrafo.V()];
+        caminho = new Pilha<Integer>();
+    }
+
+    private void dfs(Digrafo G, int v, int vd) {
+
+        // adiciona v ao caminho atual
+        caminho.empilha(v);
+        noCaminho[v] = true;
+
+        // encontrado caminho de v para vd (vértice destino)
+        if (v == vd) {
+            imprimeCaminhoAtual();
+        } // considerar todos os vizinhos que continuariam o caminho
+        else {
+            for (Aresta a : G.adj(v)) {
+                int x = a.getV2();
+                if (!noCaminho[x]) {
+                    dfs(G, x, vd);
+                }
+            }
+        }
+
+        // feita a exploração de v, então o remove do caminho
+        caminho.desempilha();
+        noCaminho[v] = false;
+    }
+
+    private void imprimeCaminhoAtual() {
+        Pilha<Integer> pilhaInvertida = new Pilha<Integer>();
+        for (int v : caminho) {
+            pilhaInvertida.empilha(v);
+        }
+        if (pilhaInvertida.tamanho() >= 1) {
+            System.out.print(pilhaInvertida.desempilha());
+        }
+        while (!pilhaInvertida.isEmpty()) {
+            System.out.print("-" + pilhaInvertida.desempilha());
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digrafo G = new Digrafo(in);
+        System.out.println("todos os caminhos simples entre 0 e 13:");
+        AlgoritimoTodosCaminhos todosCaminhos = new AlgoritimoTodosCaminhos(G, 0, 13);
+        
+
+    }
+}
--- IFES-SI-TPA-Trabalho01-2022/src/br/edu/ifes/si/tpa/AlgoritimosTodosCaminhos.java
+++
@@ -1,4 +0,0 @@
-package br.edu.ifes.si.tpa;
-public class AlgoritimosTodosCaminhos {
-    
-}
